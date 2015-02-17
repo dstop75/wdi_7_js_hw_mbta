a9e729f28cd56c5a68c4f2e5177de0b0c5cf1b82
Dan Stoppelman <dstop75@gmail.com>
2015-02-17 15:00:35 -0500

    done

--- mbta.js
+++ mbta.js
@@ -4,18 +4,67 @@ startingStation,
 endingLine,
 endingStation;
 
-startingLine = prompt("Enter the Starting Line: ");
-startingStation = prompt("Enter the Starting Station");
-endingLine = prompt("Enter the Ending Line");
-endingStation =  prompt("Enter the Ending Station");
+// commented out for testing purposes:
 
-alert("Staring at " + startingLine + " : " + startingStation);
-alert("Ending at " + endingLine + " : " + endingStation);
+// startingLine = prompt("Enter the Starting Line: ");
+// startingStation = prompt("Enter the Starting Station");
+// endingLine = prompt("Enter the Ending Line");
+// endingStation =  prompt("Enter the Ending Station");
+
+// alert("Starting at " + startingLine + " : " + startingStation);
+// alert("Ending at " + endingLine + " : " + endingStation);
 
 // Create a array for each line
+
 lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+  'green': ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"],
+  'red': ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "South Station"],
+  'orange': ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts"]
 };
 
+var calcIndexDistance = function(line, station1, station2) {
+  return Math.abs(lines[line].indexOf(station1) - lines[line].indexOf(station2));
+};
+
+var calcStops = function(originLine, origin, destinationLine, destination) {
+  if (originLine === destinationLine) {
+    return calcIndexDistance(originLine, origin, destination);
+  } else {
+    return calcIndexDistance(originLine, origin, "Park Street") + calcIndexDistance(destinationLine, destination, "Park Street");
+  }
+}
+
+startingLine = 'red';
+startingStation = 'South Station';
+endingLine = 'green';
+endingStation = 'Copley';
+
+alert("Starting at " + startingLine + " : " + startingStation + ", ending at " + endingLine + " : " + endingStation + ", should be 4: " + calcStops(startingLine, startingStation, endingLine, endingStation));
+
+startingLine = 'green';
+startingStation = 'Haymarket';
+endingLine = 'orange';
+endingStation = 'Tufts';
+
+alert("Starting at " + startingLine + " : " + startingStation + ", ending at " + endingLine + " : " + endingStation + ", should be 6: " + calcStops(startingLine, startingStation, endingLine, endingStation));
+
+startingLine = 'green';
+startingStation = 'Haymarket';
+endingLine = 'orange';
+endingStation = 'North Station';
+
+alert("Starting at " + startingLine + " : " + startingStation + ", ending at " + endingLine + " : " + endingStation + ", should be 4: " + calcStops(startingLine, startingStation, endingLine, endingStation));
+
+startingLine = 'green';
+startingStation = 'Copley';
+endingLine = 'orange';
+endingStation = 'North Station';
+
+alert("Starting at " + startingLine + " : " + startingStation + ", ending at " + endingLine + " : " + endingStation + ", should be 5: " + calcStops(startingLine, startingStation, endingLine, endingStation));
+
+startingLine = 'green';
+startingStation = 'Copley';
+endingLine = 'orange';
+endingStation = 'Tufts';
+
+alert("Starting at " + startingLine + " : " + startingStation + ", ending at " + endingLine + " : " + endingStation + ", should be 7: " + calcStops(startingLine, startingStation, endingLine, endingStation));
